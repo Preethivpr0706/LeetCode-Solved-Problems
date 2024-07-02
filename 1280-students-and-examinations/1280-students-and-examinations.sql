0daa6e1ecab763d427f7604ba6ded0c9866ad162
Preethi V <147094741+Preethivpr0706@users.noreply.github.com>
2024-07-02 21:08:05 +0530

    Delete 1280-students-and-examinations directory

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-Select s.student_id, s.student_name, sb.subject_name, Count(e.student_id) attended_exams
-From Students s
-cross join Subjects sb
-left join Examinations e
-on s.student_id = e.student_id and e.subject_name = sb.subject_name
-group by s.student_id, s.student_name,sb.subject_name
-order by s.student_id, sb.subject_name;
